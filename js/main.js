c5cd31f4e2902bd83adc2466d0712aed15701698
CMCrespo <ccrespo@wisc.edu>
2018-03-24 14:55:53 -0400

    first try
    trying to get it to work

---
+++ js/main.js
@@ -0,0 +1,8 @@
+// created by Carlos Crespo 
+
+//execute script when window is loaded
+window.onload = function(){
+
+    var container = d3.select("body") //get the <body> element from the DOM
+        .append("svg") //put new svg in the body
+};
