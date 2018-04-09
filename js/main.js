687260b7959c84af8a4a05fc8f3e2fc67e2e308a
CMCrespo <ccrespo@wisc.edu>
2018-04-08 22:38:22 -0400

    draft to final
    still need to finx axis value disparity.

--- js/main.js
+++ js/main.js
@@ -1,8 +1,422 @@
-// created by Carlos Crespo 
+// created by Carlos Crespo 2018
 
-//execute script when window is loaded
-window.onload = function(){
+(function(){
+    
+//pseudo-global variable
+var attrArray = ["Coal", "Natural Gas", "Petroleum",     "Nuclear Electric", "Hydro Electric", "Wind", "Fuel Ethanol"]; 
 
-    var container = d3.select("body") //get the <body> element from the DOM
-        .append("svg") //put new svg in the body
+var expressed = attrArray[0]; //initial attribute     
+
+//chart frame dimensions
+var chartWidth = window.innerWidth * 0.425,
+    chartHeight = 473,
+    leftPadding = 25,
+    rightPadding = 2,
+    topBottomPadding = 5,
+    chartInnerWidth = chartWidth - leftPadding - rightPadding,
+    chartInnerHeight = chartHeight - topBottomPadding * 2,
+    translate = "translate(" + leftPadding + "," + topBottomPadding + ")";
+
+//create a scale to size bars proportionally to frame and for axis
+var yScale = d3.scaleLinear()
+    .range([463, 0])
+    .domain([0, 1000]);    
+    
+// begin script when window loads
+window.onload = setMap();
+
+//set up choropleth map
+function setMap(){
+    
+    //map frame dimension
+    var width = window.innerWidth * 0.5,
+        height = 460;
+    
+    //create new svg container for map
+    var map = d3.select("body")
+        .append("svg")
+        .attr("class", "map")
+        .attr("width", width)
+        .attr("height", height)
+        //add zoom and pan functionality to map
+        .call(d3.zoom().on("zoom", function () {
+            map.attr("transform", d3.event.transform)
+        }))
+        .append("g");
+    
+    //create Albers equal area conic projection centered on US
+    var projection = d3.geoAlbersUsa()
+        .scale(800)
+        .translate([width / 2, height / 2]);
+    
+    //create path generator
+    var path = d3.geoPath()
+        .projection(projection);
+    
+    //use queue to parallelize asynchronous data loading
+    d3.queue()
+        .defer(d3.csv, "data/energy.csv") //load attributes fm csv
+        .defer(d3.json, "data/USmap.topojson") //load choropleth spatial data
+        .await(callback);
+    
+    function callback(error, csvData, states){
+        //translate states TopoJSON
+        var usStates = topojson.feature(states, states.objects.US_map).features;
+        
+        //examine results
+        console.log(error);
+        console.log(csvData);
+        console.log(states);
+              
+        //join csv data to GeoJSON enumeration units
+        usStates = joinData(usStates, csvData);
+        
+        var colorScale = makeColorScale(csvData);
+                
+        //add enumeration units to the map
+        setEnumerationUnits(usStates, map, path, colorScale);
+        
+        //add coordinated visualization to the map
+        setChart(csvData, colorScale);
+        
+        //add dropdown
+        createDropdown(csvData);
+        
+    };
 };
+
+function joinData(usStates, csvData){
+     
+    //loop through csv to assign each set of attr values to geojson region
+    for (var i = 0; i < csvData.length; i++){
+        var csvState = csvData[i]; //current region
+        var csvKey = csvState.adm1_code; //CSV primary key
+            
+    //loop through geojson regions to find correct state
+        for (var a = 0; a < usStates.length; a++){
+            var geojsonProps = usStates[a].properties; //the current region geojson properties
+            var geojsonKey = geojsonProps.adm1_code; // the geojson primary key
+                
+            //where primary keys match, transfer csv data to geojson properties object
+            if (geojsonKey == csvKey){
+                    
+                //assign all attributes and values
+                attrArray.forEach(function(attr){
+                    var val = parseFloat(csvState[attr]); //get csv attribute value
+                    geojsonProps[attr] = val; //assign attribute and value to geojson properties
+                    });
+                };
+            };
+        };
+        return usStates;
+};
+
+function setEnumerationUnits(usStates, map, path, colorScale){
+        
+    var states = map.selectAll(".states")
+        .data(usStates)
+        .enter()
+        .append("path")
+        .attr("class", function(d){
+            return "states " + d.properties.adm1_code;
+        })
+        .attr("d", path)
+        .style("fill", function(d){
+            return choropleth(d.properties, colorScale);
+        })
+        .on("mouseover", function(d){
+            highlight(d.properties);    
+        })
+        .on("mouseout", function(d){
+            dehighlight(d.properties)
+        })
+        .on("mousemove", moveLabel);
+    
+    var desc = states.append("desc")
+        .text('{"stroke": "#000", "stroke-width": "0.5px"}');
+                
+};
+    
+function makeColorScale(data){
+    var colorClasses = [
+        "#ffffcc",
+        "#c2e699",
+        "#78c679",
+        "#31a354",
+        "#006837"
+    ];
+    
+    //create color scale generator
+    var colorScale = d3.scaleThreshold()
+        .range(colorClasses);
+    
+    //build arra of all values of the expressed attribute
+    var domainArray = [];
+    for (var i = 0 ; i < data.length; i++){
+        var val = parseFloat(data[i][expressed]);
+        domainArray.push(val);
+    };
+    
+    //cluster data using ckmeans clustering algorithm to create natural breaks
+    var clusters = ss.ckmeans(domainArray, 5);
+    
+    //reset domain array to cluster minimums
+    domainArray = clusters.map(function(d){
+        return d3.min(d);
+    });
+    
+    //remove first value from domain array to create class breakpoints
+    domainArray.shift();
+    
+    //assign array of last 4 clusters minimum as domain
+    colorScale.domain(domainArray);
+    
+    return colorScale;
+};    
+
+//function to test for data value and return color
+function choropleth(props, colorScale){
+    //make sure attribute value is a number
+    var val = parseFloat(props[expressed]);
+    //if attribute value exists, assign a color; otherwise assign gray
+    if (typeof val == "number" && !isNaN(val)){
+        return colorScale(val);
+    } else {
+        return "#CCC";
+    };
+};    
+
+//function to create a coordinated bar chart
+function setChart(csvData, colorScale){
+    //create a second svg element to hold the bar chart
+    var chart = d3.select("body")
+        .append("svg")
+        .attr("width", chartWidth)
+        .attr("height", chartHeight)
+        .attr("class", "chart");
+    
+    //create a rectangle for chart background fill
+    var chartBackground = chart.append("rect")
+        .attr("class", "chartBackground")
+        .attr("width", chartInnerWidth)
+        .attr("height", chartInnerHeight)
+        .attr("transform", translate);
+    
+    //set bars for each state
+    var bars = chart.selectAll(".bars")
+        .data(csvData)
+        .enter()
+        .append("rect")
+        .sort(function(a, b){
+            return b[expressed]-a[expressed]
+        })
+        .attr("class", function(d){
+            return "bars " + d.adm1_code;
+        })
+        .attr("width", chartInnerWidth / csvData.length - 1)
+        .on("mouseover", highlight)
+        .on("mouseout", dehighlight)
+        .on("mousemove", moveLabel);
+    
+    //add style descriptor
+    var desc = bars.append("desc")
+        .text('{"stroke": "none", "stroke-width": "0px"}')
+        
+        
+        .attr("x", function (d, i) {
+            return i * (chartInnerWidth / csvData.length) + leftPadding;
+        })
+        .attr("height", function (d, i) {
+            return 463-yScale(parseFloat(d[expressed]));
+        })
+        .attr("y", function (d, i) {
+            return yScale(parseFloat(d[expressed])) ;
+        })
+        .style("fill", function (d) {
+            return choropleth(d, colorScale);
+        });
+    
+    //create a text element for the chart title
+    var chartTitle = chart.append("text")
+        .attr("x", 120)
+        .attr("y", 40)
+        .attr("class", "chartTitle")
+        .text(expressed + " Consumption per State");
+    
+    //create vertical axis generator
+    var yAxis = d3.axisLeft()
+        .scale(yScale);
+    
+    //place axis
+    var axis = chart.append("g")
+        .attr("class", "axis")
+        .attr("transform", translate)
+        .call(yAxis);
+    
+    //create frame for chart border
+    var chartFrame = chart.append("rect")
+        .attr("class", "chartFrame")
+        .attr("width", chartInnerWidth)
+        .attr("height", chartInnerHeight)
+        .attr("transform", translate);
+    
+    //set bar position, heights, and colors
+    updateChart(bars, csvData.length, colorScale);
+};
+
+//function to create a dropdown menu for attribute selection 
+function createDropdown(csvData){
+    //add select element
+    var dropdown = d3.select("body")
+        .append("select")
+        .attr("class", "dropdown")
+        .on("change", function(){
+            changeAttribute(this.value, csvData)
+        });
+    
+    //add initial option
+    var titleOption = dropdown.append("option")
+        .attr("class", "titleOption")
+        .attr("disabled", "true")
+        .text("Select Energy Source");
+    
+    //add attribute name options
+    var attrOptions = dropdown.selectAll("attrOptions")
+        .data(attrArray)
+        .enter()
+        .append("option")
+        .attr("value", function(d){ return d })
+        .text(function(d){ return d });
+};
+    
+//dropdown change listener handler
+function changeAttribute(attribute, csvData){
+    //change the expressed attribute
+    expressed = attribute;
+    
+    //recreate the color scale
+    var colorScale = makeColorScale(csvData);
+    
+    //recolor enumeration units
+    var states = d3.selectAll(".states")
+        .transition()
+        .duration(1000)
+        .style("fill", function(d){
+            return choropleth(d.properties, colorScale)
+        });
+    
+    //re-sort, resize, and recolor bars
+    var bars = d3.selectAll(".bars")
+        //re-sort bars
+        .sort(function(a, b){
+            return b[expressed] - a[expressed];
+        })
+        .transition() // add animation
+        .delay(function(d, i){
+            return i * 20
+        })
+        .duration(500);
+    
+    updateChart(bars, csvData.length, colorScale);
+};
+
+//function to position, size, and color bars in chart
+function updateChart(bars, n, colorScale){
+    //position bars
+    bars.attr("x", function(d, i){
+        return i * (chartInnerWidth / n) + leftPadding;
+    })
+    //size/resize bars
+    .attr("height", function(d, i){
+        return 463 - yScale(parseFloat(d[expressed]));
+    })
+    .attr("y", function(d, i){
+        return yScale(parseFloat(d[expressed])) + topBottomPadding;
+    })
+    //color/recolor bars
+    .style("fill", function(d){
+        return choropleth(d, colorScale);
+    });
+    
+    var chartTitle = d3.select(".chartTitle")
+        .text(expressed + " Consumption per State");
+};
+
+//function to highlight enumeration units and bars
+function highlight(props){
+    //change stroke
+    var selected = d3.selectAll("." + props.adm1_code)
+        .style("stroke", "#f03b20")
+        .style("stroke-width", "4");
+    
+    setLabel(props);
+};
+
+//function to reset the element style on mouseout
+function dehighlight(props){
+    var selected = d3.selectAll("." + props.adm1_code)
+        .style("stroke", function(){
+            return getStyle(this, "stroke")
+        })
+        .style("stroke-width", function(){
+            return getStyle(this, "stroke-width")
+        });
+    
+    function getStyle(element, styleName){
+        var styleText = d3.select(element)
+            .select("desc")
+            .text();
+        
+        var styleObject = JSON.parse(styleText);
+        
+        return styleObject[styleName];
+    };
+    
+    //remove info label
+    d3.select(".infolabel")
+        .remove();
+    
+};
+    
+//function to create dynamic label
+function setLabel(props){
+    //label content
+    var labelAttribute = "<h1>" + props[expressed] +
+        "</h1><b>" + expressed + "</b>";
+    
+    //create info label div
+    var infolabel = d3.select("body")
+        .append("div")
+        .attr("class", "infolabel")
+        .attr("id", props.adm1_code + "_label")
+        .html(labelAttribute);
+    
+    var stateName = infolabel.append("div")
+        .attr("class", "labelname")
+        .html(props.name);
+};
+    
+//function to move info label w/mouse
+function moveLabel(){
+    
+    //get width of label
+    var labelWidth = d3.select(".infolabel");
+    
+    //use coordinates of mousemove event to set label coordintes
+    var x1 = d3.event.clientX + 10,
+        y1 = d3.event.clientY - 75,
+        x2 = d3.event.clientX - labelWidth - 10,
+        y2 = d3.event.clientY + 25;
+    
+    //horizontal label coordinate, testing for overflow
+    var x = d3.event.clientX > window.innerWidth - labelWidth - 20 ? x2 : x1;
+    //vertical label coordinate, tensting for overflow
+    var y = d3.event.clientY < 75 ? y2 : y1;
+    
+    d3.select(".infolabel")
+        .style("left", x + "px")
+        .style("top", y + "px");
+    
+};
+    
+})();
--- lib/README.md
+++ lib/README.md
@@ -1,57 +1,179 @@
-# D3: Data-Driven Documents
+# d3-queue
 
-<a href="https://d3js.org"><img src="https://d3js.org/logo.svg" align="left" hspace="10" vspace="6"></a>
+A **queue** evaluates zero or more *deferred* asynchronous tasks with configurable concurrency: you control how many tasks run at the same time. When all the tasks complete, or an error occurs, the queue passes the results to your *await* callback. This library is similar to [Async.js](https://github.com/caolan/async)’s [parallel](https://github.com/caolan/async#paralleltasks-callback) (when *concurrency* is infinite), [series](https://github.com/caolan/async#seriestasks-callback) (when *concurrency* is 1), and [queue](https://github.com/caolan/async#queue), but features a much smaller footprint: as of release 2, d3-queue is about 700 bytes gzipped, compared to 4,300 for Async.
 
-**D3** (or **D3.js**) is a JavaScript library for visualizing data using web standards. D3 helps you bring data to life using SVG, Canvas and HTML. D3 combines powerful visualization and interaction techniques with a data-driven approach to DOM manipulation, giving you the full capabilities of modern browsers and the freedom to design the right visual interface for your data.
+Each task is defined as a function that takes a callback as its last argument. For example, here’s a task that says hello after a short delay:
 
-## Resources
+```js
+function delayedHello(callback) {
+  setTimeout(function() {
+    console.log("Hello!");
+    callback(null);
+  }, 250);
+}
+```
 
-* [API Reference](https://github.com/d3/d3/blob/master/API.md)
-* [Release Notes](https://github.com/d3/d3/releases)
-* [Gallery](https://github.com/d3/d3/wiki/Gallery)
-* [Examples](https://bl.ocks.org/mbostock)
-* [Wiki](https://github.com/d3/d3/wiki)
+When a task completes, it must call the provided callback. The first argument to the callback should be null if the task is successful, or the error if the task failed. The optional second argument to the callback is the return value of the task. (To return multiple values from a single callback, wrap the results in an object or array.)
+
+To run multiple tasks simultaneously, create a queue, *defer* your tasks, and then register an *await* callback to be called when all of the tasks complete (or an error occurs):
+
+```js
+var q = d3.queue();
+q.defer(delayedHello);
+q.defer(delayedHello);
+q.await(function(error) {
+  if (error) throw error;
+  console.log("Goodbye!");
+});
+```
+
+Of course, you can also use a `for` loop to defer many tasks:
+
+```js
+var q = d3.queue();
+
+for (var i = 0; i < 1000; ++i) {
+  q.defer(delayedHello);
+}
+
+q.awaitAll(function(error) {
+  if (error) throw error;
+  console.log("Goodbye!");
+});
+```
+
+Tasks can take optional arguments. For example, here’s how to configure the delay before hello and provide a name:
+
+```js
+function delayedHello(name, delay, callback) {
+  setTimeout(function() {
+    console.log("Hello, " + name + "!");
+    callback(null);
+  }, delay);
+}
+```
+
+Any additional arguments provided to [*queue*.defer](#queue_defer) are automatically passed along to the task function before the callback argument. You can also use method chaining for conciseness, avoiding the need for a local variable:
+
+```js
+d3.queue()
+    .defer(delayedHello, "Alice", 250)
+    .defer(delayedHello, "Bob", 500)
+    .defer(delayedHello, "Carol", 750)
+    .await(function(error) {
+      if (error) throw error;
+      console.log("Goodbye!");
+    });
+```
+
+The [asynchronous callback pattern](https://github.com/maxogden/art-of-node#callbacks) is very common in Node.js, so Queue works directly with many Node APIs. For example, to [stat two files](https://nodejs.org/dist/latest/docs/api/fs.html#fs_fs_stat_path_callback) concurrently:
+
+```js
+d3.queue()
+    .defer(fs.stat, __dirname + "/../Makefile")
+    .defer(fs.stat, __dirname + "/../package.json")
+    .await(function(error, file1, file2) {
+      if (error) throw error;
+      console.log(file1, file2);
+    });
+```
+
+You can also make abortable tasks: these tasks return an object with an *abort* method which terminates the task. So, if a task calls [setTimeout](https://developer.mozilla.org/en-US/docs/Web/API/WindowTimers/setTimeout) on start, it can call [clearTimeout](https://developer.mozilla.org/en-US/docs/Web/API/WindowTimers/clearTimeout) on abort. For example:
+
+```js
+function delayedHello(name, delay, callback) {
+  var id = setTimeout(function() {
+    console.log("Hello, " + name + "!");
+    callback(null);
+  }, delay);
+  return {
+    abort: function() {
+      clearTimeout(id);
+    }
+  };
+}
+```
+
+When you call [*queue*.abort](#queue_abort), any in-progress tasks will be immediately aborted; in addition, any pending (not-yet-started) tasks will not be started. Note that you can also use *queue*.abort *without* abortable tasks, in which case pending tasks will be cancelled, though active tasks will continue to run. Conveniently, the [d3-request](https://github.com/d3/d3-request) library implements abort atop XMLHttpRequest. For example:
+
+```js
+var q = d3.queue()
+    .defer(d3.request, "http://www.google.com:81")
+    .defer(d3.request, "http://www.google.com:81")
+    .defer(d3.request, "http://www.google.com:81")
+    .awaitAll(function(error, results) {
+      if (error) throw error;
+      console.log(results);
+    });
+```
+
+To abort these requests, call `q.abort()`.
 
 ## Installing
 
-If you use npm, `npm install d3`. Otherwise, download the [latest release](https://github.com/d3/d3/releases/latest). The released bundle supports anonymous AMD, CommonJS, and vanilla environments. You can load directly from [d3js.org](https://d3js.org), [CDNJS](https://cdnjs.com/libraries/d3), or [unpkg](https://unpkg.com/d3/). For example:
+If you use NPM, `npm install d3-queue`. If you use Bower, `bower install d3-queue`. Otherwise, download the [latest release](https://github.com/d3/d3-queue/releases/latest). You can also load directly from [d3js.org](https://d3js.org), either as a [standalone library](https://d3js.org/d3-queue.v3.min.js) or as part of [D3 4.0](https://github.com/d3/d3). AMD, CommonJS, and vanilla environments are supported. In vanilla, a `d3` global is exported:
 
 ```html
-<script src="https://d3js.org/d3.v4.js"></script>
+<script src="https://d3js.org/d3-queue.v3.min.js"></script>
+<script>
+
+var q = d3.queue();
+
+</script>
 ```
 
-For the minified version:
+[Try d3-queue in your browser.](https://tonicdev.com/npm/d3-queue)
 
-```html
-<script src="https://d3js.org/d3.v4.min.js"></script>
-```
+## API Reference
 
-You can also use the standalone D3 microlibraries. For example, [d3-selection](https://github.com/d3/d3-selection):
+<a href="#queue" name="queue">#</a> d3.<b>queue</b>([<i>concurrency</i>]) [<>](https://github.com/d3/d3-queue/blob/master/src/queue.js "Source")
 
-```html
-<script src="https://d3js.org/d3-selection.v1.js"></script>
-```
+Constructs a new queue with the specified *concurrency*. If *concurrency* is not specified, the queue has infinite concurrency. Otherwise, *concurrency* is a positive integer. For example, if *concurrency* is 1, then all tasks will be run in series. If *concurrency* is 3, then at most three tasks will be allowed to proceed concurrently; this is useful, for example, when loading resources in a web browser.
 
-D3 is written using [ES2015 modules](http://www.2ality.com/2014/09/es6-modules-final.html). Create a [custom bundle using Rollup](https://bl.ocks.org/mbostock/bb09af4c39c79cffcde4), Webpack, or your preferred bundler. To import D3 into an ES2015 application, either import specific symbols from specific D3 modules:
+<a href="#queue_defer" name="queue_defer">#</a> <i>queue</i>.<b>defer</b>(<i>task</i>[, <i>arguments</i>…]) [<>](https://github.com/d3/d3-queue/blob/master/src/queue.js#L20 "Source")
+
+Adds the specified asynchronous *task* callback to the queue, with any optional *arguments*. The *task* is a function that will be called when the task should start. It is passed the specified optional *arguments* and an additional *callback* as the last argument; the callback must be invoked by the task when it finishes. The task must invoke the callback with two arguments: the *error*, if any, and the *result* of the task. To return multiple results from a single callback, wrap the results in an object or array.
+
+For example, here’s a task which computes the answer to the ultimate question of life, the universe, and everything after a short delay:
 
 ```js
-import {scaleLinear} from "d3-scale";
+function simpleTask(callback) {
+  setTimeout(function() {
+    callback(null, {answer: 42});
+  }, 250);
+}
 ```
 
-Or import everything into a namespace (here, `d3`):
+If the task calls back with an error, any tasks that were scheduled *but not yet started* will not run. For a serial queue (of *concurrency* 1), this means that a task will only run if all previous tasks succeed. For a queue with higher concurrency, only the first error that occurs is reported to the await callback, and tasks that were started before the error occurred will continue to run; note, however, that their results will not be reported to the await callback.
+
+Tasks can only be deferred before [*queue*.await](#queue_await) or [*queue*.awaitAll](#queue_awaitAll) is called. If a task is deferred after then, an error is thrown. If the *task* is not a function, an error is thrown.
+
+<a href="#queue_abort" name="queue_abort">#</a> <i>queue</i>.<b>abort</b>() [<>](https://github.com/d3/d3-queue/blob/master/src/queue.js#L29 "Source")
+
+Aborts any active tasks, invoking each active task’s *task*.abort function, if any. Also prevents any new tasks from starting, and immediately invokes the [*queue*.await](#queue_await) or [*queue*.awaitAll](#queue_awaitAll) callback with an error indicating that the queue was aborted. See the [introduction](#d3-queue) for an example implementation of an abortable task. Note that if your tasks are not abortable, any running tasks will continue to run, even after the await callback has been invoked with the abort error. The await callback is invoked exactly once on abort, and so is not called when any running tasks subsequently succeed or fail.
+
+<a href="#queue_await" name="queue_await">#</a> <i>queue</i>.<b>await</b>(<i>callback</i>) [<>](https://github.com/d3/d3-queue/blob/master/src/queue.js#L33 "Source")
+
+Sets the *callback* to be invoked when all deferred tasks have finished. The first argument to the *callback* is the first error that occurred, or null if no error occurred. If an error occurred, there are no additional arguments to the callback. Otherwise, the *callback* is passed each result as an additional argument. For example:
 
 ```js
-import * as d3 from "d3";
+d3.queue()
+    .defer(fs.stat, __dirname + "/../Makefile")
+    .defer(fs.stat, __dirname + "/../package.json")
+    .await(function(error, file1, file2) { console.log(file1, file2); });
 ```
 
-In Node:
+If all [deferred](#queue_defer) tasks have already completed, the callback will be invoked immediately. This method may only be called once, after any tasks have been deferred. If this method is called multiple times, or if it is called after [*queue*.awaitAll](#queue_awaitAll), an error is thrown. If the *callback* is not a function, an error is thrown.
+
+<a href="#queue_awaitAll" name="queue_awaitAll">#</a> <i>queue</i>.<b>awaitAll</b>(<i>callback</i>) [<>](https://github.com/d3/d3-queue/blob/master/src/queue.js#L39 "Source")
+
+Sets the *callback* to be invoked when all deferred tasks have finished. The first argument to the *callback* is the first error that occurred, or null if no error occurred. If an error occurred, there are no additional arguments to the callback. Otherwise, the *callback* is also passed an array of results as the second argument. For example:
 
 ```js
-var d3 = require("d3");
+d3.queue()
+    .defer(fs.stat, __dirname + "/../Makefile")
+    .defer(fs.stat, __dirname + "/../package.json")
+    .awaitAll(function(error, files) { console.log(files); });
 ```
 
-You can also require individual modules and combine them into a `d3` object using [Object.assign](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/assign):
-
-```js
-var d3 = Object.assign({}, require("d3-format"), require("d3-geo"), require("d3-geo-projection"));
-```
+If all [deferred](#queue_defer) tasks have already completed, the callback will be invoked immediately. This method may only be called once, after any tasks have been deferred. If this method is called multiple times, or if it is called after [*queue*.await](#queue_await), an error is thrown. If the *callback* is not a function, an error is thrown.
